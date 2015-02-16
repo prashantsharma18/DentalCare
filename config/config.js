a8430be787c0bd25c6013bc3471fe5156b54c5c5
prashantsharma18 <prashantsharma18@gmail.com>
2015-02-16 07:39:10 +0530

    glob code made it sync

--- config/config.js
+++ config/config.js
@@ -33,24 +33,14 @@ module.exports.getGlobbedFiles = function (globPatterns, removeRoot) {
         if (urlRegex.test(globPatterns)) {
             output.push(globPatterns);
         } else {
-            var mg = new glob.Glob(globPatterns, { sync: true }, function (err, files) {
-                if (removeRoot) {
-                    files = files.map(function (file) {
-                        return file.replace(removeRoot, '');
-                    });
-                }
-                
-                output = _.union(output, files);
-            });
-            //glob(globPatterns, {sync: true}, function (err, files) {
-            //    if (removeRoot) {
-            //        files = files.map(function (file) {
-            //            return file.replace(removeRoot, '');
-            //        });
-            //    }
-                
-            //    output = _.union(output, files);
-            //});
+            
+            var files = glob.sync(globPatterns);
+            if (removeRoot) {
+                files = files.map(function (file) {
+                    return file.replace(removeRoot, '');
+                });
+            }
+            output = _.union(output, files);
         }
     }
     
